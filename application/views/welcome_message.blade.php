b8081bd2b3e028ff54095365622a3aa28768f9e9
awanz <hermawan9815@gmail.com>
2020-06-01 04:06:09 +0700

    add blade

--- application/views/welcome_message.blade.php
+++ application/views/welcome_message.blade.php
@@ -74,7 +74,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>The page you are looking at is being generated dynamically by CodeIgniter & Awan.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code>application/views/welcome_message.blade.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
